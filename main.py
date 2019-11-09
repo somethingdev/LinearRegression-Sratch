f4b66669be9c4bc3b5e4f0f4cbedbb7841a9576f
Marcelo Arias <47821093+360macky@users.noreply.github.com>
2019-11-08 20:49:17 -0500

    Equations: Summatory and Variance

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 class LinearRegression():
     def __init__(self, x, y, n):
         self.x = x
@@ -23,3 +25,19 @@ class LinearRegression():
         for i in range(0, len(array)):
             summatory += array[i]
         return summatory
+
+    
+annual_rent = [12, 15, 20, 25, 30]
+holiday_spending = [1, 1.5, 2, 3, 4]
+n_elements = 5
+
+lr = LinearRegression(annual_rent, holiday_spending, n_elements)
+
+summatory_x = lr.get_sum_of_array(annual_rent)
+summatory_y = lr.get_sum_of_array(holiday_spending)
+
+mean_x = summatory_x / n_elements
+mean_y = summatory_y / n_elements
+  
+variance_x = math.sqrt((lr.get_sum_of_array(lr.get_pow_x())) / n_elements - pow(mean_x, 2))
+variance_y = math.sqrt((lr.get_sum_of_array(lr.get_pow_y())) / n_elements - pow(mean_y, 2))
